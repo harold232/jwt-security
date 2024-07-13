ff0e6003183ccbc82ece455b4e371494bdbafcb3
Harold Aquino <harold.aquino@unmsm.edu.pe>
2024-07-13 01:19:33 -0500

    User repository

--- src/main/java/com/hacode/security/user/UserRepository.java
+++ src/main/java/com/hacode/security/user/UserRepository.java
@@ -1,2 +1,7 @@
-package com.hacode.security.user;public interface UserRepository {
+package com.hacode.security.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
