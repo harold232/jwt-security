b3f786d89da967361065e33257e57b62da842b43
Harold Aquino <harold.aquino@unmsm.edu.pe>
2024-07-13 02:18:16 -0500

    Filter Auth.

--- src/main/java/com/hacode/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/hacode/security/config/JwtAuthenticationFilter.java
@@ -15,10 +15,13 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -26,5 +29,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
